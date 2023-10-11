f14fbcb271b44f684425b627b9b4a8d95da6ac28
Михаил Калинин <65865966+KleachiM@users.noreply.github.com>
2023-10-11 17:49:19 +0300

    Update 9.sql

--- lw5/queries/9.sql
+++ lw5/queries/9.sql
@@ -7,4 +7,5 @@ LEFT JOIN payment p ON r.rental_id = p.rental_id
 JOIN film_category fc ON f.film_id = fc.film_id
 JOIN category c ON fc.category_id = c.category_id AND c.name = 'Horror'
 GROUP BY f.film_id
+  
 ORDER BY revenue DESC;
